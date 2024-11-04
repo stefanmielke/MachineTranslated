fa8e91eee9d3624cf0ed55e52e1f19f044fe4704
Stefan Mielke <stefanmielke@fastmail.com>
2024-11-04 18:25:20 -0300

    making the jp text whiter when disabling en text

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,9 +1,14 @@
+off = true;
+
 $(window).on('load', function() {
     $('#btn-jp').on('click', function() {
         $('em').slideToggle();
         $('h2').slideToggle();
     });
     $('#btn-en').on('click', function() {
+        off = !off
+
         $('h4').slideToggle();
+        $('em').css('color', off ? 'gray' : '#f0e7d5');
     });
 });
