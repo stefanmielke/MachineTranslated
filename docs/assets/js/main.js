d11e064cedc5a74b292f66358d6296c186592431
Stefan Mielke <stefanmielke@fastmail.com>
2024-11-04 18:26:52 -0300

    also 'whiter' the title

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -9,6 +9,7 @@ $(window).on('load', function() {
         off = !off
 
         $('h4').slideToggle();
+        $('h2').css('color', off ? 'gray' : '#f0e7d5');
         $('em').css('color', off ? 'gray' : '#f0e7d5');
     });
 });
